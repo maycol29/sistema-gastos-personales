8553de214098df9628b2b24d3f6ebae4a2d87671
maycol29 <maycolramirez847@gmail.com>
2026-06-14 00:12:01 -0500

    Delete Nueva carpeta (2) directory

--- Nueva carpeta (2)/Nueva carpeta/src/services/reporteService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/reportes";
-
-export const obtenerGastosPorCategoria = () => {
-    return axios.get(`${API_URL}/gastos-por-categoria`);
-};
